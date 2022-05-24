6c0743d64224471ac727471f814c9778d10fed8c
lukaszjakobczak <lukaszjakobczak@gmail.com>
2022-05-24 10:01:02 +0200

    Repo initialization

---
+++ src/main/java/com/lukaszjakobczak/roompccupancymanager/RoompccupancymanagerApplication.java
@@ -0,0 +1,13 @@
+package com.lukaszjakobczak.roompccupancymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoompccupancymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoompccupancymanagerApplication.class, args);
+	}
+
+}
