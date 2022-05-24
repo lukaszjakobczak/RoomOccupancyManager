c7689d295ee1a8e6343a9ae1d5c0dcc58b5b1d7a
lukaszjakobczak <lukaszjakobczak@gmail.com>
2022-05-24 10:57:40 +0200

    Added controller test

---
+++ src/test/groovy/roompccupancymanager/BaseIntegrationSpec.groovy
@@ -0,0 +1,20 @@
+package roompccupancymanager
+
+import com.lukaszjakobczak.roompccupancymanager.RoompccupancymanagerApplication
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.web.server.LocalServerPort
+import org.springframework.test.web.servlet.MockMvc
+import spock.lang.Specification
+
+@AutoConfigureMockMvc
+@SpringBootTest(classes = [RoompccupancymanagerApplication], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BaseIntegrationSpec extends Specification {
+
+    @Autowired
+    protected MockMvc mockMvc
+
+    @LocalServerPort
+    protected int port
+}
---
+++ src/test/groovy/roompccupancymanager/infrastructure/endpoint/RoomOccupancyManagerControllerIntegrationSpec.groovy
@@ -0,0 +1,30 @@
+package roompccupancymanager.infrastructure.endpoint
+
+import roompccupancymanager.BaseIntegrationSpec
+
+import static org.springframework.http.MediaType.APPLICATION_JSON
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+
+class RoomOccupancyManagerControllerIntegrationSpec extends BaseIntegrationSpec {
+
+    private static final String URL = "/rooms/optimize"
+
+    def "Should return OK response when endpoint is invoked"() {
+        when: "Endpoint is invoked"
+            def response = mockMvc.perform(
+                    post(URL)
+                    .contentType(APPLICATION_JSON)
+                    .content(guestsOrdersJson())
+            ).andReturn().response
+        then: "HTTP 204 is returned"
+            response.status == 204
+    }
+
+    private String guestsOrdersJson() {
+        """
+            {
+                "orders": [23, 45, 155, 374, 22, 99.99, 100, 101, 115, 209]
+            }
+        """
+    }
+}
